79dbbc4d8d32fc5b5da014c20da23fa9f81ecb85
XHawk87 <hawk87@hotmail.co.uk>
2025-01-05 00:52:34 +0000

    #42 Cleaned up commented code

--- filters.py
+++ filters.py
@@ -6,12 +6,6 @@ def to_lua(obj):
     elif isinstance(obj, dict):
         lua_items = [f'{key} = {to_lua(value)}' for key, value in obj.items()]
         return "{ " + ", ".join(lua_items) + " }"
-        #lua_table = "{ "
-        #for key, value in obj.items():
-        #    lua_value = to_lua(value)  # Recursively convert nested dicts
-        #    lua_table += f'{key} = {lua_value}, '
-        #lua_table += " }"
-        #return lua_table
     else:
         return str(obj)
 
