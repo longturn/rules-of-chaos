c8d6adaa7e9605180b9b26c8e2277ae8640fb2ff
XHawk87 <hawk87@hotmail.co.uk>
2025-01-05 00:50:29 +0000

    #42 Restored parser.lua, improved generated lua format, and moved generation to script.lua.j2

--- filters.py
+++ filters.py
@@ -4,12 +4,14 @@ def to_lua(obj):
     elif isinstance(obj, bool):
         return str(obj).lower()
     elif isinstance(obj, dict):
-        lua_table = "{"
-        for key, value in obj.items():
-            lua_value = to_lua(value)  # Recursively convert nested dicts
-            lua_table += f'{key} = {lua_value},'
-        lua_table += "}"
-        return lua_table
+        lua_items = [f'{key} = {to_lua(value)}' for key, value in obj.items()]
+        return "{ " + ", ".join(lua_items) + " }"
+        #lua_table = "{ "
+        #for key, value in obj.items():
+        #    lua_value = to_lua(value)  # Recursively convert nested dicts
+        #    lua_table += f'{key} = {lua_value}, '
+        #lua_table += " }"
+        #return lua_table
     else:
         return str(obj)
 
